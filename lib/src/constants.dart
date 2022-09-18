afad32152d986acb4838127446d2b0cc927a8db3
Kaio C. de Oliveira <hello@kaio.dev>
2022-09-18 14:35:43 -0300

    add: constants

---
+++ lib/src/constants.dart
@@ -0,0 +1,23 @@
+import 'dart:ui';
+
+class Constants {
+  //Colors
+  static const Color defaultBackGroundColor = Color(0xFFFFFAFA);
+
+  //Padding, Margin, Spacing
+  static const double zero = 0.0;
+  static const double extraSmall = 2.0;
+  static const double small = 4.0;
+  static const double medium = 8.0;
+  static const double large = 16.0;
+  static const double extraLarge = 32.0;
+
+  //TODO: Assets
+  static const String designPatternsDataPath =
+      'assets/data/design_patterns.json';
+  static const String markdownPath = 'assets/markdown';
+
+  //Routes
+  static const String initialRoute = '/';
+  static const categoryRoute = '/category';
+}
