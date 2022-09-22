6592e3c59be9e9efcd5663a616043a0628bb873a
Kaio C. de Oliveira <hello@kaio.dev>
2022-09-22 16:28:32 -0300

    update: add introduction into routes

--- lib/src/routes.dart
+++ lib/src/routes.dart
@@ -1,8 +1,12 @@
 import 'package:appy/src/constants.dart';
+import 'package:appy/src/data/models/design_pattern.dart';
 import 'package:appy/src/data/models/design_pattern_category.dart';
 import 'package:appy/src/pages/category/category.dart';
+import 'package:appy/src/pages/design_pattern_details/design_pattern_details.dart';
 import 'package:appy/src/pages/home/home.dart';
 import 'package:flutter/material.dart';
+
+import 'widgets/introduction/introduction.dart';
 
 class Routes {
   static Route<dynamic> generateRoute(RouteSettings settings) {
@@ -15,6 +19,12 @@ class Routes {
             category: settings.arguments as DesignPatternCategory,
           ),
         );
+      case "/intro":
+        return MaterialPageRoute(
+            builder: (_) => DesignPatternDetails(
+                  designPattern: settings.arguments as DesignPattern,
+                  example: const Introduction(),
+                ));
       default:
         return MaterialPageRoute(builder: (_) => Home());
     }
