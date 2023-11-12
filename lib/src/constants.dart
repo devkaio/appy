e8f0df3aa29c627457b7f87a1f531f80d8a81118
Kaio C. de Oliveira <hello@kaio.dev>
2023-11-12 09:18:37 -0300

    update constants

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -5,17 +5,29 @@ class Constants {
   static const Color defaultBackGroundColor = Color(0xFFFFFAFA);
 
   //Padding, Margin, Spacing
+  /// 0.0px
   static const double zero = 0.0;
+
+  /// 2.0px
   static const double extraSmall = 2.0;
+
+  /// 4.0px
   static const double small = 4.0;
+
+  /// 8.0px
   static const double medium = 8.0;
+
+  /// 16.0px
   static const double large = 16.0;
+
+  /// 24.0px
   static const double mediumLarge = 24.0;
+
+  /// 32.0px
   static const double extraLarge = 32.0;
 
   //Assets
-  static const String designPatternsDataPath =
-      'assets/data/design_patterns.json';
+  static const String designPatternsDataPath = 'assets/data/design_patterns.json';
   static const String markdownPath = 'assets/markdown';
 
   //Routes
